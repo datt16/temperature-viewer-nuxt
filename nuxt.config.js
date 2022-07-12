d1e426adedf049baa15e339a8ac405607ed3d75e
Beckham_home <39327357+Beckham31415@users.noreply.github.com>
2022-07-12 11:51:24 +0900

    faviconとページ名の変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,14 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s',
-    title: '温度確認アプリ',
+    title: 'umihime',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.svg' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
